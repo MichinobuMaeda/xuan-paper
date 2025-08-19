6245fbca2459fe489b7eba577e103f5196aaad78
Michinobu Maeda <michinobumaeda@gmail.com>
2025-08-19 23:53:01 +0900

    test: scrolling threshold #2

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ function App() {
   const [lastScrollY, setLastScrollY] = useState(0);
 
   window.addEventListener("scroll", () => {
-    if (Math.abs(window.scrollY - lastScrollY) > 32) {
+    if (Math.abs(window.scrollY - lastScrollY) > 64) {
       if (lastScrollY !== 0) {
         setScrollDirection(window.scrollY - lastScrollY);
       }
