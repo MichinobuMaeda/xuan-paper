886fbe66c21f72a93aff2d72b9d8928cbbc590c0
Michinobu Maeda <michinobumaeda@gmail.com>
2025-09-15 03:13:22 +0000

    fix: correct file extensions for component imports

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 
 // Export all React components
 export { default as AppBar } from "./lib/AppBar.jsx";
-export { default as Button } from "./lib/Button.js";
+export { default as Button } from "./lib/Button.jsx";
 export { default as ButtonGroup } from "./lib/ButtonGroup.jsx";
 export { default as CheckBox } from "./lib/CheckBox.jsx";
 export { default as Fab } from "./lib/Fab.jsx";
@@ -14,11 +14,11 @@ export { default as NavigationBar } from "./lib/NavigationBar.jsx";
 export { default as NavigationDrawer } from "./lib/NavigationDrawer.jsx";
 export { default as NavigationRail } from "./lib/NavigationRail.jsx";
 export { default as PasswordField } from "./lib/PasswordField.jsx";
-export { default as PWABadge } from "./lib/PWABadge.js";
+export { default as PWABadge } from "./lib/PWABadge.jsx";
 export { default as RadioGroup } from "./lib/RadioGroup.jsx";
 export { default as Slider } from "./lib/Slider.jsx";
-export { default as Switch } from "./lib/Switch.js";
-export { default as TextField } from "./lib/TextField.js";
+export { default as Switch } from "./lib/Switch.jsx";
+export { default as TextField } from "./lib/TextField.jsx";
 export { default as ToggleDarkModeButton } from "./lib/ToggleDarkModeButton.jsx";
 export { default as ToggleLanguageButton } from "./lib/ToggleLanguageButton.jsx";
 
